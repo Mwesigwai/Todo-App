fb8037f2dca27ae857b9f369dd16310b5f846eb2
isaac <mwesigwai433@gmail.com>
2024-07-30 10:33:24 +0300

    added a dialog for delete action

--- editAndDeleteButtons/handleDeleteAction.js
+++ editAndDeleteButtons/handleDeleteAction.js
@@ -6,16 +6,30 @@ export function handleDeleteAction(event) {
     const taskNameSpan = event.target.parentElement.parentElement.children[0].children[1];
     let taskContainer = event.target.parentElement.parentElement;
     const todoItemDisplay = document.querySelector('.todoItemsDisplay');
+    const deleteDialog = document.querySelector('#deleteDialog');
+    
+    const okButton = document.getElementById('dialogOkBtn');
+    const cancelButton = document.getElementById('cancel');
     const taskName = taskNameSpan.textContent;
+
     const tasks = getTasks();
-    const index = getIndex(taskName, tasks);
-    todoItemDisplay.removeChild(taskContainer)
+    deleteDialog.showModal();
 
-    tasks.splice(index,1);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    cancelButton.addEventListener('click',()=>{
+        deleteDialog.close();
+    });
 
-    updateStatusBar();
-    updatePagination();
+    okButton.addEventListener('click', ()=>{
+        deleteDialog.close();
+        const index = getIndex(taskName, tasks);
+        todoItemDisplay.removeChild(taskContainer)
+
+        tasks.splice(index,1);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
+        updateStatusBar();
+        updatePagination();
+    });
 }
 
 function getIndex(taskName, tasks) {
